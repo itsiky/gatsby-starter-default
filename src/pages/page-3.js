816b8e8507ea9a4560aee7e31c5e1a569eb93b7b
Itsik <itsiky@gmail.com>
2020-12-31 11:39:54 +0200

    Update page-3.js

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -25,22 +25,7 @@ const conntact = () => (
   </p>
 </form>
   <p></p>
-    <Link to="/">Go back to the homepage</Link>
-  
-  document.querySelector("form").addEventListener("submit", handleSubmit);
-
-  const handleSubmit = (e) => {
-  e.preventDefault()
-  let myForm = document.getElementById('pizzaOrder');
-  let formData = new FormData(myForm)
-  fetch('/', {
-    method: 'POST',
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString()
-    }).then(() => console.log('Form successfully submitted')).catch((error) =>
-        alert(error))
-  }
-  
+    <Link to="/">Go back to the homepage</Link> 
   </Layout>
 )
 export default conntact
