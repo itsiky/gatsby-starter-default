c1c0a9ed52b5e7c6d04ec7a0516996179e460e11
Itsik <itsiky@gmail.com>
2020-12-31 11:10:51 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to my new site.</p>
-    <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
