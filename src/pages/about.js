5e3032a29a6ac7bffa97633e68f5e84b66c55e97
itsiky <itsiky@gmail.com>
2021-01-03 16:56:12 +0200

    updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import SharkImg from "..components/shark"
+import SharkImg from "..components/sharkimg"
 
 export default function About() {
   return (
