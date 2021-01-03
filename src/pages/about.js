113ba6d7ddd82308034689bf7679fab775256f45
itsiky <itsiky@gmail.com>
2021-01-03 12:30:09 +0200

    update

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,18 @@
 import React from "react"
-import Header from "../components/header"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
 
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
       <Header headerText="About Gatsby" />
       <p>Such wow. Very React.</p>
+      <p></p>
+      <Link to="/">Go back to the homepage</Link> 
     </div> 
+    
   )
 }
