856bdf6e830c029c60409b1ece88e5d616a1f7f2
itsiky <itsiky@gmail.com>
2021-01-31 18:36:14 +0200

    .

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,8 +9,6 @@ const NotFoundPage = () => (
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     <p></p>
-    <Link to="/">Go back to the homepage</Link>
-    
   </Layout>
 )
 
